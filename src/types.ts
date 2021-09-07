d83db2b4e12e9a439c12ae5f74cf4abebca58ba5
Emilio Bondioli <e.bondioli@todo.to.it>
2021-09-07 17:18:04 +0200

    update types

--- src/types.ts
+++ src/types.ts
@@ -10,7 +10,7 @@ export interface ControllerConfig {
         baudRate?: number
     }
     mock: boolean,
-    rate: number;
+    debug: boolean
 };
 
 export interface ClientConfig {
@@ -20,6 +20,7 @@ export interface ClientConfig {
     }
     stage: StageConfig,
     mock: boolean,
+    debug: boolean
 };
 
 export interface StageConfig {

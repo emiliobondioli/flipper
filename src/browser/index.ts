2c8e1491d37b0071bc9ce2cb585f203d4d5d7adb
Emilio Bondioli <e.bondioli@todo.to.it>
2022-01-28 14:30:58 +0100

    use add stage named export to browser package

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -1,6 +1,5 @@
 /** @module flipper/browser */
 
-import _stage from './stage'
 export { Client } from './client'
 export { Simulator } from './simulator'
-export const Stage = _stage
+export { Stage } from './stage'
